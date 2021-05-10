b54e9305a55ed123d457534f5692421f8c921ead
Huynh Hoang Huy <kr.nzk2002@gmail.com>
2021-05-10 20:58:16 +0700

    Finish and upload the project

---
+++ src/Clock.js
@@ -0,0 +1,79 @@
+import React from "react";
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+
+    const time = Date.parse(this.props.deadline) - Date.parse(new Date());
+    if (time < 0) {
+      this.setState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+    } else {
+      const seconds = Math.floor((time / 1000) % 60);
+      const minutes = Math.floor((time / 1000 / 60) % 60);
+      const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+      const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+      this.state = {
+        days: days,
+        hours: hours,
+        minutes: minutes,
+        seconds: seconds,
+      };
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.getTimeTillDeadline(this.props.deadline);
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    if (this.interval) clearInterval(this.interval);
+  }
+
+  getTimeTillDeadline(deadline) {
+    const time = Date.parse(deadline) - Date.parse(new Date());
+    if (time < 0) {
+      this.setState({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+    } else {
+      const seconds = Math.floor((time / 1000) % 60);
+      const minutes = Math.floor((time / 1000 / 60) % 60);
+      const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+      const days = Math.floor(time / (1000 * 60 * 60 * 24));
+
+      this.setState({ days, hours, minutes, seconds });
+    }
+  }
+
+  leadingZero(value) {
+    return value < 10 ? "0" + value : value;
+  }
+
+  render() {
+    const { days, hours, minutes, seconds } = this.state;
+
+    return (
+      <div className="clock-container">
+        <div className="clock-item">
+          {this.leadingZero(days)}
+          <span>{days === 1 ? "day" : "days"}</span>
+        </div>
+        <div className="clock-item">
+          {this.leadingZero(hours)}
+          <span>{hours === 1 ? "hour" : "hours"}</span>
+        </div>
+        <div className="clock-item">
+          {this.leadingZero(minutes)}
+          <span>{minutes === 1 ? "minute" : "minutes"}</span>
+        </div>
+        <div className="clock-item">
+          {this.leadingZero(seconds)}
+          <span>{seconds === 1 ? "second" : "seconds"}</span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Clock;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Countdown.css";
+import Countdown from "./Countdown";
+import reportWebVitals from "./reportWebVitals";
+
+const date = {
+  dateName: "The F-Code Birthday",
+  deadline: "2021-05-25T00:00:00+07:00"
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Countdown
+      deadline={date.deadline}
+      dateName={date.dateName}
+    />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
